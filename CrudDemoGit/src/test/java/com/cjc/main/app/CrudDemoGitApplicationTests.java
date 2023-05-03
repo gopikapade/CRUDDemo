459045cafba7da1dc1b2a5dff2f7aae3d0ee9237
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-03 12:00:49 +0530

    Crud Project Intialisation

---
+++ CrudDemoGit/src/test/java/com/cjc/main/app/CrudDemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.main.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
